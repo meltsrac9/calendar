146d0ac6189cb879d836ae3d1d6133ce2d2a0c3f
meltsrac9 <rachel.meltser@gmail.com>
2018-07-01 16:07:34 -0500

    gasp! calendar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+import "./App.css";
+
+import Calendar from './components/calendar';
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Testy mcTest</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Calendar />
       </div>
     );
   }
