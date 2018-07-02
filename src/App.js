cbd17608f711d45e5bd1c67a3c352853b4f55bf6
meltsrac9 <rachel.meltser@gmail.com>
2018-07-02 18:49:20 -0500

    egads! a calendar has appeared -added fontawesome, css styles and calendar mock up

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import "./App.css";
 
 import Calendar from './components/calendar';
 
+
+const style = {
+  position: 'relative',
+  margin: '50px auto'
+}
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Calendar />
+        <Calendar style = {style} width = '300px' />
       </div>
     );
   }
